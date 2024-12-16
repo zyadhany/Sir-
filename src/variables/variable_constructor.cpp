19e86bf2ce20d5d736f50b0168f1eebc5b2b3bf9
Ramyrashad127 <ramy41599@gmail.com>
2024-12-16 20:44:52 +0200

    add validation function for expresion

--- src/variables/variable_constructor.cpp
+++ src/variables/variable_constructor.cpp
@@ -1,24 +1,11 @@
 #include "variable.h"
-/**
-
- * Variable()
-
-    Variable("(n + 20) * m");
-    Variabele("n") + 
-
-
-
-    Variable("n") + Va
- */
-Variable::Variable(const string &expresion){
-
-    *this = Variable("temp", "str", expresion);
-}
-
 
 /**
- * 
+ * check if the expresion is valid by checking if the barnaces are valid
+ * @param expresion
+ * @return
  */
+vector<string> operations = {"+", "-", "*", "/", "%", "&", "|", "^", "<<", ">>", "<", ">", "<=", ">=", "==", "!=", "&&", "||", "!", "~"};
 bool IsValidBarnaces(string expresion)
 {
     int barnaces = 0;
@@ -35,3 +22,77 @@ bool IsValidBarnaces(string expresion)
     }
     return barnaces == 0;
 }
+
+/**
+ * check if string has valid number of double quotes
+ * @param str string
+ * @return bool
+ */
+bool check_double_quotes(string str) {
+    bool check = false;
+    for (int i = 0; i < str.size(); i++) {
+        if(str[i] == '\\' && str[i + 1] == '\"' && check) i++;
+        else if(str[i] == '\"') check = !check;
+    }
+    return !check;
+}
+/**
+ * trim the spaces from the string
+ * @param str
+ * @return string
+ */
+string trim_spaces(string str) {
+    string temp = "";
+    bool check = false;
+    for (int i = 0; i < str.size(); i++) {
+        if(str[i] == '\\' && str[i + 1] == '\"' && check)
+        {
+            i++;
+            continue;
+        }
+        if(str[i] == '\"')
+            check = !check;
+        if(check)
+            continue;
+        if (str[i] != ' ') {
+            temp += str[i];
+        }
+    }
+    return temp;
+}
+
+/**
+
+ * Variable()
+
+    Variable("(n + 20) * m");
+    Variabele("n") + 
+
+
+
+    Variable("n") + Va
+ */
+Variable::Variable(const string &expresion){
+    expresion = trim_spaces(expresion);
+    bool check = false;
+    for (auto opp : operations) {
+        if (expresion.find(opp) != string::npos) {
+            check = true;
+            break;
+        }
+    }
+    if (!check)
+    {
+        if ()
+        *this = Variable("temp", "str", expresion);
+        return;
+
+    }
+    if (!IsValidBarnaces(expresion))
+        throw runtime_error("Invalid barnaces");
+    
+    
+    // *this = Variable("temp", "str", expresion);
+}
+
+
